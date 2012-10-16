e1a22b2a8dc43575bab6be8bc7a4daa05748f29a
ok100 <ok100@lavabit.com>
2012-10-16 21:31:08 +0200

    Cleanup

--- homepage.py
+++ homepage.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+########## begin config ##########
 
-### begin config ###
-
-output = 'homepage.html'           # output html file
-title = 'Speed Dial'               # title of the page
-font = ['Monospace', '14px']       # font
-separator = '>'                    # separator between group title and links
-bgcolor = '#020202'                # background color
-fgcolor = '#999999'                # foreground color
-lnkcolor = '#999999'               # link color
-hdrcolor = '#B3B3B3'               # group title color
-sepcolor = '#4C4C4C'               # separator color
+output = 'homepage.html'      # output file
+title = 'Speed Dial'          # page title
+font = ('Monospace', '14px')  # font
+separator = '>'               # separator between group title and links
+colors = (
+    '#020202',  # background
+    '#999999',  # links
+    '#B3B3B3',  # group title
+    '#4C4C4C',  # separator
+)
 
 # add your links here
 links = {
@@ -31,11 +30,12 @@ links = {
     ],
 }
 
-### end config ###
+########## end config ##########
 
-css_template = '''body {
+import os
+
+css = '''body {
   background-color: %s;
-  color: %s;
   font-family: "%s";
   font-size: %s;
   font-weight: normal;
@@ -61,36 +61,31 @@ td:first-child {
 td:nth-child(2) {
   font-weight: normal;
   color: %s;
-}''' % (bgcolor, fgcolor, font[0], font[1],
-        lnkcolor, lnkcolor, hdrcolor, sepcolor)
+}''' % (colors[0], font[0], font[1], colors[1], colors[1], colors[2], colors[3])
 
-links_template = '<table valign="middle" border="0" \
-                  width="100%" height="100%"><tr><td><table>'
+links_html = ''
 for group in sorted(links):
-    links_template += '<tr><td align="right">%s</td> \
-                       <td>%s</td><td>' % (group, separator)
+    links_html += '<tr><td align="right">%s</td><td>%s</td><td>' % (group, separator)
     for site in sorted(links[group]):
-        links_template += '<a href="%s">%s</a> ' % (site[1], site[0])
-    links_template += '</td></tr>'
-links_template += '</table></td></tr></table>'
+        links_html += '<a href="%s">%s</a> ' % (site[1], site[0])
+    links_html += '</td></tr>'
 
-html_template = '''<html>
+html = '''<html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>%s</title>
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-  %s
+  <table valign="middle" border="0" width="100%%" height="100%%"><tr><td><table>
+    %s
+  </table></td></tr></table>
 </body>
-</html>''' % (title, links_template)
+</html>''' % (title, links_html)
 
-f = open(output, 'w')
-f.write(html_template)
-f.close()
-
-f = open('style.css', 'w')
-f.write(css_template)
-f.close()
+with open(output, 'w') as file:
+    file.write(html)
+with open('style.css', 'w') as file:
+    file.write(css)
 
 os.system('tidy -utf8 -i -m -q -asxhtml %s' % output)
